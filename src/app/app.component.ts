d8a9d96e31a5ab0504654bfabf33f29dd9f6ce68
kemoik <kemoicaleb17@gmail.com>
2022-06-13 16:16:03 +0300

    Added Event Binding in the navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shoppingApp';
+    //  storing which feature should be displayed 
+  loadedFeature = 'recipe';
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
